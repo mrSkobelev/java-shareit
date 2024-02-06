0cbc47094c8263c7aea279118caea091f1aac523
Alex <lelik-ttk@yandex.ru>
2024-02-06 14:54:06 +0300

    fix: pom fixed

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -9,5 +9,5 @@ public class ShareItApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
 	}
-
+	//этот коммент для коммита по исправлению pom. Потом удалю))
 }
